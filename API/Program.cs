7a0a3a0fe483586f3169397321039b9a299743ce
Mateus-Kiwi <mat.dalla2004@gmail.com>
2023-10-11 10:29:54 -0300

    Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -30,4 +30,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception exception)
+{
+    logger.LogError(exception, "Error during migration");
+}
+
 app.Run();
