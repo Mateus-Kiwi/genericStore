9ebad3aaaaf3c5c07f695d97901b96435a16e5ed
Mateus-Kiwi <mat.dalla2004@gmail.com>
2023-10-23 15:12:21 -0300

    Pagination and sorting

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,22 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BannerComponent } from './banner/banner.component'
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     BannerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule,
+    ShopModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
