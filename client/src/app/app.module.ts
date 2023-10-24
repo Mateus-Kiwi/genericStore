eea3fdc6be83ee7864c9f305167c84b567b35b1b
Mateus-Kiwi <mat.dalla2004@gmail.com>
2023-10-24 14:25:24 -0300

    Error handling done

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BannerComponent } from './banner/banner.component'
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { RouterModule } from '@angular/router';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 
 @NgModule({
@@ -25,7 +26,9 @@ import { RouterModule } from '@angular/router';
     HomeModule,
     
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
