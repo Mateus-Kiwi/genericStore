0842bc92904235a1f7156532a85d94f8da276738
Mateus-Kiwi <mat.dalla2004@gmail.com>
2023-11-06 11:27:15 -0300

    Better routing and auth

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, AsyncValidatorFn, FormBuilder, Validators } from '@angular/forms';
 import { AccountService } from '../account.service';
 import { Router } from '@angular/router';
+import { debounceTime, finalize, map, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -10,19 +11,37 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent {
 
+  errors: string[] | null = null;
+
   constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router){}
 
   complexPassword = "(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$"
   
   registerForm = this.fb.group({
     displayName: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
+    email: ['', [Validators.required, Validators.email], [this.validateEmailNotTaken()]],
     password: ['', [Validators.required, Validators.pattern]],
   })
 
   onSubmit(){
     this.accountService.register(this.registerForm.value).subscribe({
-      next: () => this.router.navigateByUrl('/')
+      next: () => this.router.navigateByUrl('/'),
+      error: error => this.errors = error.errors
     })
   }
+
+  validateEmailNotTaken(): AsyncValidatorFn {
+    return(control: AbstractControl) => {
+      return control.valueChanges.pipe(
+        debounceTime(1000),
+        take(1),
+        switchMap(() => {
+          return this.accountService.checkEmailExists(control.value).pipe(
+            map(result  => result ? {emailExists: true} : null),
+            finalize(() => control.markAsTouched)
+          )
+        })
+      )
+    }
+  } 
 }
